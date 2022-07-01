15c41ed7d7a87637fe69723495be3af2ba6b1619
iChris <87505413+iChris473@users.noreply.github.com>
2022-07-02 00:28:00 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,15 @@ import {getStorage} from "firebase/storage"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyA-bV1pXQcKj3C_9Nby63eL-E1CxmlbrJM",
-    authDomain: "unimeet-f07f9.firebaseapp.com",
-    projectId: "unimeet-f07f9",
-    storageBucket: "unimeet-f07f9.appspot.com",
-    messagingSenderId: "370813903611",
-    appId: "1:370813903611:web:ab887523dad5a3e0e5c65e"
-  };
+  apiKey: "AIzaSyDQtha2RsD-RrOTOEQOSg7r7qESk2h2rcY",
+  authDomain: "aluta-1470f.firebaseapp.com",
+  projectId: "aluta-1470f",
+  storageBucket: "aluta-1470f.appspot.com",
+  messagingSenderId: "758095462939",
+  appId: "1:758095462939:web:75af7048ff1a82c2416ebe"
+};
+
+
 
   // Initialize Firebase
 const app = initializeApp(firebaseConfig)
